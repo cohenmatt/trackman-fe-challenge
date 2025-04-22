f05b6ce8e347199ec098686bca721cc38b6d69d4
Matt Cohen <matt@whyhive.com>
2025-04-22 13:24:54 +0100

    Form submission creates new facility

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,6 @@
 import { TextField, Typography } from "@mui/material";
 
 export default function Input({id, label}) {
-    console.log(label);
     return(
         <>
         <Typography variant="body2">{label}</Typography>
--- src/pages/FacilityEdit.jsx
+++ src/pages/FacilityEdit.jsx
@@ -1,21 +1,48 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Button, Stack, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import Input from "../components/Input";
+import { useForm, Controller } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { addFacility } from "../features/facilityList/facilityListSlice";
 
 export default function FacilityEdit() {
+    const { control, handleSubmit } = useForm();
     const goTo = useNavigate();
+    const dispatch = useDispatch();
+    const onSubmit = (data) => {
+        dispatch(addFacility(data));
+        alert(JSON.stringify(data));
+    };
 
     return(
-        <Box>
-            <Typography variant="h3">Create/Edit</Typography>
-            <Stack direction="column" spacing={2}>
-                <Typography variant="body1" fontWeight="bold">Facility Information</Typography>
-                <Input id="facility-name" label="Facility name" />
-                <Input id="address" label="Address" />
-                <Input id="description" label="Description" />
-                <Input id="img-url" label="Cover Image URL" />
-            </Stack>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack direction="column" spacing={2} sx={{p: 2}}>
+            <Controller
+                name="id"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                    <TextField {...field} label="ID" variant="outlined" />
+                )}
+            />
+            <Controller
+                name="name"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                    <TextField {...field} label="Name" variant="outlined" />
+                )}
+            />
+            <Controller
+                name="address"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                    <TextField {...field} label="Address" variant="outlined" />
+                )}
+            />
+            <Button type="submit">Submit</Button>
             <Button variant="contained" onClick={() => goTo("/")}>Back</Button>
-        </Box>
-    );
+            </Stack>
+        </form>
+    )
 }
